f6197e256a4715fef252b2fb2d8ac10fb7dc71a2
ramighaz <113072730+ramighaz@users.noreply.github.com>
2025-03-24 07:13:54 +0300

    Update index.jsx

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,11 +1,18 @@
 "use client";
 import { BtnList } from "@/app/data";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavButton from "./NavButton";
 
 const Navigation = () => {
+    const [radius, setRadius] = useState(0);
+
+    useEffect(() => {
+        // Only access window object after the component is mounted (client-side)
+        const radiusValue = window.innerWidth * 0.2 - 16; // Convert '20vw - 1rem' to pixels
+        setRadius(radiusValue);
+    }, []); // Empty dependency array ensures this runs only once after component mounts
+
     const angleIncrement = (2 * Math.PI) / BtnList.length; // Full circle in radians
-    const radius = window.innerWidth * 0.2 - 16; // Convert '20vw - 1rem' to pixels
 
     return (
         <div className="fixed h-screen flex items-center justify-center w-full">
@@ -15,9 +22,7 @@ const Navigation = () => {
                     const x = radius * Math.cos(angleRad);
                     const y = radius * Math.sin(angleRad);
 
-                    return <NavButton  key={btn.label} x={x} y={y} {...btn}/>
-                        
-                    
+                    return <NavButton key={btn.label} x={x} y={y} {...btn} />;
                 })}
             </div>
         </div>
